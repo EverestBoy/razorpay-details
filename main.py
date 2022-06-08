33dbd7d634b7d3dc2f06f5a587d99dc0a3d634f9
EverestBoy <galabwot@gmail.com>
2022-06-08 18:05:16 +0530

    finished

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from audioop import add
 from typing import Optional
+from unicodedata import name
+from pecan import response
 import razorpay
 from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request
+from datetime import datetime as dt
 
 
 app = FastAPI()
@@ -12,24 +16,32 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+success_add_class = 'show-success-add'
+failure_add_class = 'show-failure-add'
+
+success_delete_class = 'show-success-delete'
+failure_delete_class = 'show-failure-delete'
+
+secretKeyMain = "secret"
+
+
 
 def getPaymentInformation(paymentId):
-    success = "failure"
-    message = "api key doesnot exist"
+    message = "failure, api key doesnot exist"
     amount = ""
     paymentId = paymentId.strip()
+    success, message, date, email, contact, address, merchantOrderId, accountId, amount, tax = None, None, None, None, None, None, None, None, None, None
     try:
         keyFile = open("keyvalue.txt", 'r')
         keyText = keyFile.read()
         keyFile.close()
 
         keyText = keyText.split("\n")
+        print(keyText)
 
         clientList = []
 
-        print("getting len")
         for keys in keyText:
-            print(keys)
             if keys.strip() != "":
                 try:
                     keyvalue =keys.split("#")
@@ -39,24 +51,27 @@ def getPaymentInformation(paymentId):
                 except Exception as e:
                     print(e)
 
-        print(len(clientList))
-
         for client in clientList:
             try:
-                print(paymentId)
+                message = "success"
                 response = client.payment.fetch(paymentId)
                 print(response)
-                success = "success"
-                message = str(response)
-                amount = "INR "+str(response["amount"]/100.0)
+                email = response["email"]
+                date = dt.fromtimestamp(response["created_at"])
+                contact = response["contact"]
+                address = response["notes"]["address"]
+                merchantOrderId = response["notes"]["merchant_order_id"]
+                accountId = response["account_id"]
+                amount = response["currency"]+" "+str(response["amount"]/100.0)
+                tax = response["tax"]
                 break
             except Exception as e:
+                print(paymentId)
                 print(e)
-                success = "failure"
-                message = e
+                message = "failure "+ str(e)
     except Exception as e:
         print(e)
-    return success, message, amount
+    return  message, paymentId, date, email, contact, address, merchantOrderId, accountId, amount, tax
 
 
 
@@ -75,8 +90,21 @@ def form_post(request: Request):
 
 @app.post("/form")
 def form_post(request: Request, paymentId: str = Form(...)):
-    result, message, amount = getPaymentInformation(paymentId=paymentId)
-    return templates.TemplateResponse('form.html', context={'request': request, 'result': result, 'amount':amount, 'message':message, 'showResult':'show'})
+    message, paymentId, date, email, contact, address, merchantOrderId, accountId, amount, tax = getPaymentInformation(paymentId)
+    return templates.TemplateResponse('form.html', context={
+            'request': request, 
+            'id': paymentId,
+            'date': str(date),
+            'email': email,
+            'contact': contact,
+            'address': address,
+            'merchantOrderId': merchantOrderId,
+            'accountId': accountId,
+            'amount':amount, 
+            'tax': tax,
+            'message':message, 
+            'showResult':'show'
+        })
 
 @app.get("/setapi")
 def form_post(request: Request):
@@ -104,3 +132,77 @@ def set_api(
     keyFile.close()
     
     return templates.TemplateResponse('success.html', context={'request': request, 'message': message})
+
+@app.get("/addkey")
+def form_post(request: Request):
+    return templates.TemplateResponse('addKey.html', context={'request': request, 'responseClass':'hide', 'responseMessage':''})
+
+def addKey(key, value):
+    print(f"adding key and value {key} {value}")
+    keyFile = open("keyvalue.txt", 'a+')
+    keyFile.write(f"{key.strip()}#{value.strip()}\n")
+    keyFile.close()
+    return 'key and value added successfully', success_add_class
+
+@app.post("/addkey")
+def add_api(
+        request: Request, 
+        secretKey: str= Form(default=""), 
+        key: str= Form(default=""),  
+        value: Optional[str] = Form(default="")
+    ):
+    responseClass = failure_add_class
+    message = "secret key donot match"
+    
+    if secretKey == "secret":
+        message = "key and value successfully added"
+        if key =="" or value == "":
+            message = "please provide both key and value"
+        else:
+            message, responseClass = addKey(key, value)
+    
+    return templates.TemplateResponse('addKey.html', context={'request': request, 'responseClassAdd': responseClass, 'responseMessageAdd': message, 'responseClassDelete': 'hide', 'responseMessageDelete': '',})
+
+
+def deleteKey(key):
+    message = "given key doesnot exist"
+    response = failure_delete_class
+    file = open("keyvalue.txt", 'r')
+    content = file.read()
+    file.close()
+    contentList = content.split("\n")
+    newContent = []
+    for contentItem in contentList:
+        contentItem = contentItem.strip()
+        if contentItem != "":
+            contentItemSplitted = contentItem.split("#")
+            print(contentItemSplitted)
+            if contentItemSplitted[0] == key:
+                message = f"key: {key} successfully deleted"
+                response = success_delete_class
+            else: 
+                newContent.append(contentItem)
+    
+    print(newContent)
+    newContent = "\n".join(newContent)
+    file = open("keyvalue.txt", 'w')
+    content = file.write(newContent)
+    file.close()
+
+    return message, response
+
+
+@app.post("/deletekey")
+def delete_api(
+        request: Request, 
+        secretKey: str= Form(default=""), 
+        key: str= Form(default=""),  
+        value: Optional[str] = Form(default="")
+    ):
+    message, responseClass = "", ""
+    if secretKey != secretKeyMain:
+        message, responseClass = "secrete key donot match", failure_delete_class
+    else:
+        message, responseClass = deleteKey(key=key)
+    
+    return templates.TemplateResponse('addKey.html', context={'request': request, 'responseClassDelete': responseClass, 'responseMessageDelete': message, 'responseClassAdd': 'hide', 'responseMessageAdd':''})
